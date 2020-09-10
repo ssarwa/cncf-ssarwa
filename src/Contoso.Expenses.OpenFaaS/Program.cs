9862f33e76ed698fd79051571b5778ded0ac0e84
Srikant Sarwa <ssarwa@microsoft.com>
2020-09-10 08:25:36 -0700

    Update Program.cs

--- src/Contoso.Expenses.OpenFaaS/Program.cs
+++ src/Contoso.Expenses.OpenFaaS/Program.cs
@@ -15,7 +15,6 @@ namespace Contoso.Expenses.OpenFaaS
         {
             CreateHostBuilder(args).Build().Run();
         }
-
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
